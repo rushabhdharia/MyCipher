f8a4ecd723141d1df426556c5b5b72b4a3ce39f9
rushabhdharia <rushabh.dharia@gmail.com>
2017-08-29 22:21:28 +0530

    added Ceasar Cipher and comments

--- Cipher.java
+++ Cipher.java
@@ -2,41 +2,56 @@ import java.util.Scanner;
 public class Cipher {
   public static void main(String args[])
   {
-    int i;
+    //int i;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the plain text");
     String plainText = sc.next();
+    String encryptText = encrypt(plainText);
+    decrypt(encryptText);
+    
+  }
+
+  public static String encrypt(String plainText)
+  {
+    int i;
     int n = plainText.length();
     char text[] = plainText.toCharArray();
     for(i=0;i<n;i++)
     {
-      text[i]-=65;
-      text[i]+=text[i];
+      text[i]-=65; // ASCII to Alpha
+      text[i]+=3; // Ceasar Cipher encryption
+      text[i]+=text[i]; 
       if(text[i]>26)
-      text[i]-=26;  
+        text[i]-=26;  
       else
-      text[i]+=1;
-      text[i]+=65;
+        text[i]+=1;
+      text[i]+=65; //alpha to ASCII
       
     }
     String encrypt = String.valueOf(text);
     System.out.println("Encrypted text = "+encrypt);
-    
+    return encrypt;
+  }
+
+  public static void decrypt(String encryptText)
+  {
+
+    int i;
+    int n = encryptText.length();
+    char text[] = encryptText.toCharArray();
     System.out.print("Decrypted text = ");
     for(i=0;i<n;i++)
     {
-      //char ij = 'I';
-      int test=Character.getNumericValue(text[i])-9;
+      int test=Character.getNumericValue(text[i])-9 ;//Character.getNumericValue() returns value from 10-35 for A-Z. Therefore -9 is used. 
       
       if(test%2!=0)
       {
-        test += 27;
-         
+        test += 27; //if test is odd we add the value by 27 to make it even.    
       }
-      test = test/2 + 64; 
+      test = test/2; 
+      test += 64; // Convert back to ASCII
+      test -= 3; // Ceasar Cipher decryption
       System.out.print((char)test);
     } 
-    //String decrypt = String.valueOf(text);
-    //System.out.println("Decrypted text = "+decrypt);
   }
 }
